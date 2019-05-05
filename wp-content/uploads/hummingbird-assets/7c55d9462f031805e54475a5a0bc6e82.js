7c075f747a88ee11bfa4ff5e0fc2dfa94daefb7d
barsadigital <50280767+barsadigital@users.noreply.github.com>
2019-05-05 16:11:54 -0300

    Teste

---
+++ wp-content/uploads/hummingbird-assets/7c55d9462f031805e54475a5a0bc6e82.js
@@ -0,0 +1,8 @@
+/**handles:wprss_custom**/
+jQuery( document ).ready( function() { 
+if(jQuery.fn.colorbox) {
+    jQuery( 'a.colorbox' ).colorbox(
+    {iframe:true, width:'80%', height:'80%'}
+    );
+}
+});
