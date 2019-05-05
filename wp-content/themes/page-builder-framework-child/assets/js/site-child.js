056446663640bef0c02cf818dfb6b96574a43983
barsadigital <50280767+barsadigital@users.noreply.github.com>
2019-05-05 16:02:55 -0300

    ok

--- wp-content/themes/page-builder-framework-child/assets/js/site-child.js
+++
@@ -1,5 +0,0 @@
-(function($) {
-
-	// custom script goes here
-
-})( jQuery );
--- wp-content/uploads/hummingbird-assets/7c55d9462f031805e54475a5a0bc6e82.js
+++
@@ -1,8 +0,0 @@
-/**handles:wprss_custom**/
-jQuery( document ).ready( function() { 
-if(jQuery.fn.colorbox) {
-    jQuery( 'a.colorbox' ).colorbox(
-    {iframe:true, width:'80%', height:'80%'}
-    );
-}
-});
